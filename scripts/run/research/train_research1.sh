b884f7f5ee82953efe42fde4572e9d86100f91f4
kinderst <kinders@uw.edu>
2024-06-16 02:55:14 -0600

    add bash

---
+++ scripts/run/research/train_research1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --time=23:59:00
+#SBATCH --partition=aa100
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=16
+#SBATCH --nodes=1
+#SBATCH --output=train-research-1.%j.out
+
+module purge
+
+module load anaconda
+conda activate dinov2
+
+echo "== Starting! =="
+sleep 3
+
+cd /projects/skinder@xsede.org/repos/dinov2_cervical
+
+sleep 3
+
+torchrun dinov2/train/train.py \
+    --config-file /projects/skinder@xsede.org/repos/dinov2_cervical/dinov2/configs/train/vitb14_research.yaml \
+    --output-dir /scratch/alpine/skinder@xsede.org/cervix_dinov2/outputs/research/run_1
+
+echo "== End =="
