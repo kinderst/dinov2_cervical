2dafd6128a8e8ec1b23da9dc95aea1519448772f
kinderst <kinders@uw.edu>
2024-07-09 00:32:58 -0600

    prevents entering distributed mode for external use

--- dinov2/utils/config.py
+++ dinov2/utils/config.py
@@ -47,7 +47,7 @@ def get_cfg_from_args(args):
 
 
 def default_setup(args):
-    distributed.enable(overwrite=True)
+    # distributed.enable(overwrite=True)
     seed = getattr(args, "seed", 0)
     rank = distributed.get_global_rank()
 
